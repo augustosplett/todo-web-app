6f74ee51ab816d8645d449d0b93fb2a0871dfb77
Augusto Madeira Splett <augusto.splett@gmail.com>
2024-07-13 14:03:36 -0400

    Feature: implemented the logic to change the text style based on the task status

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,23 +3,34 @@
 import styles from "./Task.module.css";
 import React, { useState } from 'react';
 
-export default function Task({ title, description, isdone }){
+export default function Task({ title, description, isdone }) {
     const [done, setDone] = useState(isdone);
+    const [style, setStyle] = useState(isdone ? "line-through" : "none");
 
     const handleCheckboxChange = (event) => {
-      setDone(event.target.checked);
+        const checked = event.target.checked;
+        setDone(checked);
+        changeStyle(checked);
     };
 
-    return(
+    const changeStyle = (checked) => {
+        if (checked) {
+            setStyle("line-through");
+        } else {
+            setStyle("none");
+        }
+    };
+
+    return (
         <div className={styles.taskContainer}>
             <input 
-            className={styles.check}
-            type="checkbox" 
-            checked={done} 
-            onChange={handleCheckboxChange} 
+                className={styles.check}
+                type="checkbox" 
+                checked={done} 
+                onChange={handleCheckboxChange} 
             />
             <div className={styles.textContainer}>
-                <h3 className={styles.title}>{title}</h3>
+                <h3 className={styles.title} style={{ textDecoration: style }}>{title}</h3>
                 <p className={styles.description}>{description}</p>
             </div>
         </div>
